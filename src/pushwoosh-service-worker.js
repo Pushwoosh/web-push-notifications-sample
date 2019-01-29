3d9c027af1bbdf08cb9990aa75652e9229e7c314
Michael Shmakov <phobos@arello-mobile.com>
2019-01-29 11:42:56 +0700

    fix for the cdn caching

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,1 +1,1 @@
-importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js');
+importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search);
